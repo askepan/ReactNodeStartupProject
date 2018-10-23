277791272dcf93a5371ac43d64db4f91ef2853a9
Johan Nåtby <johan.natby@tretton37.com>
2018-10-23 15:37:43 +0200

    Added some starter React Components, eslint and testSetup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About Page</h1>
+                <p>Added this just to have a react link on the page</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/tests/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('A test is needed for the npm script to work', () => {
+    it('always pass', () => {
+        expect(true).toEqual(true);
+    });
+});
